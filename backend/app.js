ec4194398f48714de0052b1e2e1bd697854975ba
shray_gla_git <shray.gupta_cs21@gla.ac.in>
2024-09-21 23:07:57 +0530

    dply

--- backend/app.js
+++ backend/app.js
@@ -1,39 +1,28 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
 const cors = require("cors");
-const routes  = require('./src/Routes/route')
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const routes = require('./src/Routes/route');
+
+const app = express();
 
 const corsOptions = {
-    // origin: "http://localhost:5173",
-    origin:  'https://swadehindustan.netlify.app' || process.env.Frontend_URL,
+    // origin: process.env.FRONTEND_URL || 'https://swadehindustan.netlify.app',
+    origin: 'https://swadehindustan.netlify.app',
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
 };
-// app.use(cors());
+
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // Allow preflight requests
 
-// app.use((req,res,next)=>{
-//   res.header("Access-Control-Allow-Origin", 'https://swadehindustan.netlify.app' || process.env.Frontend_URL);
-//   res.header("Access-Control-Allow-Methods", "ORIGIN , X-Requested-With,Content-Type, Accept");
-//   next();
-// })
- 
-
-app.use(bodyParser.json({limit: '100mb'}));
-app.use(bodyParser.urlencoded({limit: '100mb', extended: true}));
-app.use(express.json());
-app.use(routes)
-
-
-
+app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+app.use(routes);
 
 const PORT = process.env.PORT || 8001;
-require('./src/db/conn') //for connecting to mongodb
+require('./src/db/conn'); // for connecting to mongodb
 
-
-
-  app.listen(PORT,(req,res)=>{
-    console.log(`listening on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
 });
